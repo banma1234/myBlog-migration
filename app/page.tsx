94a18eed0c52993eed7bd643d92316914aa51827
banma1234 <banma1234@gmail.com>
2023-08-09 18:43:33 +0900

    feat: 메인페이지

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>한글</h1>
+      <h2>테스트</h2>
+      <h3>!@#$$$^&)())+&*%^</h3>
+      <p>Lorem Ipsum is...</p>
+    </div>
+  );
+}
