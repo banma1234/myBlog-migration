52dcc969548cb7d6cdf7e80d3cd39a0611d0a885
banma1234 <banma1234@gmail.com>
2023-08-11 18:53:32 +0900

    feat: index board에 필요한 데이터 fetch

---
+++ app/api/posts/GET/viewIndex.ts
@@ -0,0 +1,34 @@
+import { NextResponse } from "next/server";
+import { connectToDatabase } from "util/mongodb";
+
+export default async function viewIndex() {
+  try {
+    const { db } = await connectToDatabase();
+    const options = {
+      sort: { uploadDate: -1 },
+      projection: {
+        _id: 0,
+        title: 1,
+        uploadDate: 1,
+        thumbnail: 1,
+      },
+    };
+
+    const res = await db
+      .collection("posts")
+      .find({}, options)
+      .limit(6)
+      .toArray();
+
+    return NextResponse.json({
+      data: res,
+      success: true,
+    });
+  } catch (e: any) {
+    console.log(e);
+    return NextResponse.json({
+      data: e,
+      success: false,
+    });
+  }
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,30 @@
+import { use } from "react";
+
+async function getPost() {
+  const myHeaders = new Headers({
+    "Content-Type": "text/html; charset=utf-8",
+  });
+  myHeaders.append("viewType", "VIEW_INDEX");
+
+  const res = await fetch(`${process.env.DEV_URL}/api/posts`, {
+    method: "GET",
+    headers: myHeaders,
+    next: { revalidate: 10 },
+  });
+  const data = await res.json();
+
+  return data.data;
+}
+
 export default function Home() {
+  const posts = use(getPost());
+
   return (
     <div>
-      <h1>한글</h1>
-      <h2>테스트</h2>
-      <h3>!@#$$$^&)())+&*%^</h3>
-      <p>Lorem Ipsum is...</p>
+      <h1>hello</h1>
+      {posts.map((item: any, i: number) => {
+        return <p key={i}>{item.title}</p>;
+      })}
     </div>
   );
 }
