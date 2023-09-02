6aead24bd54fa484bea1394ff9f1658b6488d7b1
chocoham <banma1234@gmail.com>
2023-09-02 23:43:52 +0900

    feat: 화살표 좌, 우 아이콘 추가

--- util/iconHandler.tsx
+++ util/iconHandler.tsx
@@ -1,9 +1,11 @@
-/* icon list = {
-    mail, github, enter, like, menu,
-    cancel, sun, moon, copy, check
- } */
-
-export default function iconHandler(type: string, size: string) {
+/*
+ * icon list = { mail, github, enter, like, menu, arrow, cancel, sun, moon, copy, check }
+ */
+export default function iconHandler(
+  type: string,
+  size: string,
+  color?: string
+) {
   switch (type) {
     case "mail":
       return (
@@ -60,19 +62,35 @@ export default function iconHandler(type: string, size: string) {
           <path d="m8 6.236-.894-1.789c-.222-.443-.607-1.08-1.152-1.595C5.418 2.345 4.776 2 4 2 2.324 2 1 3.326 1 4.92c0 1.211.554 2.066 1.868 3.37.337.334.721.695 1.146 1.093C5.122 10.423 6.5 11.717 8 13.447c1.5-1.73 2.878-3.024 3.986-4.064.425-.398.81-.76 1.146-1.093C14.446 6.986 15 6.131 15 4.92 15 3.326 13.676 2 12 2c-.777 0-1.418.345-1.954.852-.545.515-.93 1.152-1.152 1.595L8 6.236zm.392 8.292a.513.513 0 0 1-.784 0c-1.601-1.902-3.05-3.262-4.243-4.381C1.3 8.208 0 6.989 0 4.92 0 2.755 1.79 1 4 1c1.6 0 2.719 1.05 3.404 2.008.26.365.458.716.596.992a7.55 7.55 0 0 1 .596-.992C9.281 2.049 10.4 1 12 1c2.21 0 4 1.755 4 3.92 0 2.069-1.3 3.288-3.365 5.227-1.193 1.12-2.642 2.48-4.243 4.38z" />
         </svg>
       );
+    case "arrowLeft":
+      return (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width={size}
+          height={size}
+          fill={color}
+          className="bi bi-arrow-left-square"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm11.5 5.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"
+          />
+        </svg>
+      );
     case "arrowRight":
       return (
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width={size}
           height={size}
-          fill="currentColor"
-          className="bi bi-chevron-right"
+          fill={color}
+          className="bi bi-arrow-right-square"
           viewBox="0 0 16 16"
         >
           <path
             fill-rule="evenodd"
-            d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+            d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"
           />
         </svg>
       );
