43f1388d571d43cf5b358a2018e4aa12a0342523
chocoham <banma1234@gmail.com>
2023-09-04 00:16:12 +0900

    design: 카드 위로 뜨는 애니메이션 추가

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import "../styles/cardStyle.scss";
 
 export function CardLayout(props: any) {
@@ -7,33 +8,59 @@ export function CardLayout(props: any) {
     <div className="card_layout">
       {posts &&
         posts.map((item: any, i: number) => {
+          const url = `/posts/${item.postId}`;
+
           return (
-            <div className="card" key={i}>
-              {/* <div className="card_thumbnail">
-                <Image
-                  src={item.thumbnail}
-                  alt="card Img"
-                  width={380}
-                  height={250}
-                  style={{
-                    maxWidth: "100%",
-                    height: "auto",
-                  }}
-                />
-              </div> */}
-              <h3>{item.title}</h3>
-              <p>{item.uploadDate}</p>
-            </div>
+            <Link href={url} key={i}>
+              <div className="card">
+                <div className="card_thumbnail">
+                  <Image
+                    src={item.thumbnail}
+                    alt="card Img"
+                    width={380}
+                    height={250}
+                    style={{
+                      maxWidth: "100%",
+                      height: "auto",
+                    }}
+                  />
+                </div>
+                <div className="card_info">
+                  <h3 className="card_title">{item.title}</h3>
+                  <p>{item.uploadDate}</p>
+                </div>
+              </div>
+            </Link>
           );
         })}
     </div>
   );
 }
 
-export function Card(post: any) {
+export function CardHead(props: any) {
+  const post = props.post;
+  const url = `/posts/${post.postId}`;
+
   return (
-    <div className="card_head">
-      <h1>damn</h1>
-    </div>
+    <Link href={url}>
+      <div className="card_head">
+        <div className="card_thumbnail">
+          <Image
+            src={post.thumbnail}
+            alt="card Img"
+            width={1200}
+            height={789}
+            style={{
+              maxWidth: "100%",
+              height: "auto",
+            }}
+          />
+        </div>
+        <div className="card_head_info">
+          <h1 className="card_head_title">{post.title}</h1>
+          <p>{post.uploadDate}</p>
+        </div>
+      </div>
+    </Link>
   );
 }
