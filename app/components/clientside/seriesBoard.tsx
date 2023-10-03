02abc447b9720e33535f67fcdce2b508bc940e89
banma1234 <banma1234@gmail.com>
2023-10-03 18:16:37 +0900

    design: 시리즈 보드 디자인

--- app/components/clientside/seriesBoard.tsx
+++ app/components/clientside/seriesBoard.tsx
@@ -1,29 +1,62 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import iconHandler from "util/iconHandler";
 import Link from "next/link";
+import "../../styles/seriesBoardStyle.scss";
 
-export default function SeriesBoard(props: { data: Array<any> }) {
+export default function SeriesBoard(props: {
+  data: Array<any>;
+  postId: number;
+}) {
   const [isClick, setIsClick] = useState<boolean>(false);
-  const data = props.data;
+  const [show, setShow] = useState<boolean>(false);
+  const dropDownName = `container_${isClick}`;
+
+  useEffect(() => {
+    if (isClick) {
+      setShow(true);
+    } else {
+      setTimeout(() => {
+        setShow(false);
+      }, 380);
+    }
+  }, [isClick]);
 
   return (
     <>
-      <div onClick={() => setIsClick(!isClick)}>
-        <h2>시리즈별로 모아보기</h2>
+      <div className="seriesBoard">
+        <div
+          className="seriesBoard_dropDown"
+          onClick={() => setIsClick(!isClick)}
+        >
+          <h2>
+            📋 {"[ "}
+            {props.data[0].series}
+            {" ]"} 시리즈 몰아보기
+          </h2>
+          {iconHandler(isClick ? "arrowUp" : "arrowDown", "32")}
+        </div>
+
+        {show && (
+          <div className={dropDownName}>
+            <ul className="container_ul">
+              {props.data.map((item: any, i: number) => {
+                const url = `/posts/${item.postId}`;
+                const postName =
+                  props.postId === item.postId ? "current" : "others";
+                return (
+                  <Link href={url}>
+                    <li className={postName} key={i}>
+                      {item.title}
+                    </li>
+                  </Link>
+                );
+              })}
+            </ul>
+          </div>
+        )}
       </div>
-      {isClick && (
-        <ul>
-          {data.map((item: any, i: number) => {
-            const url = `/posts/${item.postId}`;
-            return (
-              <Link href={url}>
-                <li key={i}>{item.title}</li>
-              </Link>
-            );
-          })}
-        </ul>
-      )}
     </>
   );
 }
