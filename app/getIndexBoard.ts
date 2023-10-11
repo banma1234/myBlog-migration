127f722c394c59b1d057ea72b53f344e9d91ccd1
banma1234 <banma1234@gmail.com>
2023-10-11 14:29:20 +0900

    refactor: 불필요 구문 제거

--- app/getIndexBoard.ts
+++ app/getIndexBoard.ts
@@ -12,18 +12,3 @@ export default async function getIndexBoard() {
 
   return { posts: data };
 }
-
-// async function getSeries() {
-//   const myHeaders = new Headers({
-//     "Content-Type": "text/html; charset=utf-8",
-//   });
-//   myHeaders.append("viewType", "VIEW_SERIES");
-
-//   const res = await fetch(`${process.env.DEV_URL}/api/posts`, {
-//     method: "GET",
-//     headers: myHeaders,
-//   });
-//   const { data } = await res.json();
-
-//   return { sereis: data };
-// }
