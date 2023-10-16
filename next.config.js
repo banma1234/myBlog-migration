f5eaada50f55cd26c33cd343e2631d90830f88c0
chocoham <banma1234@gmail.com>
2023-10-17 00:00:19 +0900

    build: google auth 주소 추가

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["choco-image-server.cdn.ntruss.com", "localhost"],
+    domains: [
+      "choco-image-server.cdn.ntruss.com",
+      "localhost",
+      "lh3.googleusercontent.com",
+    ],
   },
 };
 
