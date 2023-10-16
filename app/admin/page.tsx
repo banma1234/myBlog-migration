ff4392f7374f32e3cc1f9fb2f833488ee20e8566
chocoham <banma1234@gmail.com>
2023-10-17 01:32:50 +0900

    feat: admin만 접근 가능하도록 middleware 구현

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,15 +1,33 @@
-"use client";
-import { useSession } from "next-auth/react";
-
-export default function Admin() {
-  const { data: session, status } = useSession();
-
+export default async function Admin() {
   return (
     <div>
-      {session && <h1>hi {session.user?.email || `nothing`}</h1>}
-      {!session && <h1>No!!!</h1>}
-      <button onClick={() => console.log(session)}>show session</button>
-      <button onClick={() => console.log(status)}>show status</button>
+      <h1>Login bitch</h1>
+      <h2>how can i help you?</h2>
+      <p>it's admin page</p>
     </div>
   );
 }
+
+// "use client";
+
+// import { useSession } from "next-auth/react";
+
+// export default function Admin() {
+//   const { data: session, status } = useSession();
+
+//   return (
+//     <div>
+//       <h1>Login bitch</h1>
+//       <h2>how can i help you?</h2>
+//       {session && session.user && (
+//         <>
+//           <h1>hi {session.user.name}</h1>
+//           <p>{session.user.email}</p>
+//         </>
+//       )}
+//       {!session && <h1>No!!!</h1>}
+//       <button onClick={() => console.log(session)}>show session</button>
+//       <button onClick={() => console.log(status)}>show status</button>
+//     </div>
+//   );
+// }
