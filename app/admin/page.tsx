7f0dac1eafa326eb0ce72ffb94617b1b15aac28d
chocoham <banma1234@gmail.com>
2023-10-19 23:30:10 +0900

    fix: 불필요 구문 제거

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -7,27 +7,3 @@ export default async function Admin() {
     </div>
   );
 }
-
-// "use client";
-
-// import { useSession } from "next-auth/react";
-
-// export default function Admin() {
-//   const { data: session, status } = useSession();
-
-//   return (
-//     <div>
-//       <h1>Login bitch</h1>
-//       <h2>how can i help you?</h2>
-//       {session && session.user && (
-//         <>
-//           <h1>hi {session.user.name}</h1>
-//           <p>{session.user.email}</p>
-//         </>
-//       )}
-//       {!session && <h1>No!!!</h1>}
-//       <button onClick={() => console.log(session)}>show session</button>
-//       <button onClick={() => console.log(status)}>show status</button>
-//     </div>
-//   );
-// }
--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,3 @@
-// import { getServerSession } from "next-auth/next";
-// import { authConfig } from "app/auth/auth";
-// import { NextResponse } from "next/server";
-// import { Session } from "next-auth";
 import { withAuth } from "next-auth/middleware";
 
 export default withAuth({
@@ -14,44 +10,3 @@ export default withAuth({
 });
 
 export const config = { matcher: ["/admin/:path*"] };
-
-// export async function middleware(req: NextRequest) {
-//   const session = await getServerSession(authConfig);
-//   const { pathname } = req.nextUrl;
-
-//   if (pathname.startsWith("/admin")) {
-//     console.log("middleware active - admin");
-
-//     await withAuth(session);
-//   }
-
-//   if (pathname.startsWith("/auth")) {
-//     console.log("middleware active - login");
-
-//     await withOutAuth(session);
-//   }
-// }
-
-// async function withAuth(session: Session | null) {
-//   try {
-//     if (session && session.userRole != "admin") {
-//       return NextResponse.redirect("/");
-//     }
-//   } catch (e: unknown) {
-//     console.log(e);
-//     throw new Error("token didn't exist");
-//   }
-// }
-
-// async function withOutAuth(session: Session | null) {
-//   try {
-//     if (session && session.user) {
-//       return NextResponse.redirect("/");
-//     }
-//   } catch (e: unknown) {
-//     console.log(e);
-//     throw new Error("session didn't exist");
-//   }
-// }
-
-// export const config = { matcher: ["/admin/(.*)"] };
