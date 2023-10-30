241aeb23c4b80bd027e649a5e8c4d56a4298f442
banma1234 <banma1234@gmail.com>
2023-10-30 17:33:29 +0900

    refactor: profile 아이콘 변경

--- util/iconHandler.tsx
+++ util/iconHandler.tsx
@@ -231,14 +231,10 @@ export default function iconHandler(
           width={size}
           height={size}
           fill="currentColor"
-          className="bi bi-person-circle"
+          className="bi bi-person-fill-lock"
           viewBox="0 0 16 16"
         >
-          <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-          <path
-            fill-rule="evenodd"
-            d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
-          />
+          <path d="M11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm-9 8c0 1 1 1 1 1h5v-1a1.9 1.9 0 0 1 .01-.2 4.49 4.49 0 0 1 1.534-3.693C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4Zm7 0a1 1 0 0 1 1-1v-1a2 2 0 1 1 4 0v1a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-4a1 1 0 0 1-1-1v-2Zm3-3a1 1 0 0 0-1 1v1h2v-1a1 1 0 0 0-1-1Z" />
         </svg>
       );
     case "launch":
