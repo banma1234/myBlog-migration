b9f24c383c0e67af330dc7f0c3e7aa0a655430b8
chocoham <banma1234@gmail.com>
2023-11-26 18:21:14 +0900

    refactor: promise 객체 분리

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,12 @@ import { CardLayout, Banner } from "./components/card";
 import { getIndexBoard, getRecommendPost } from "./utils";
 
 export default async function Home() {
-  const [newest, recommend] = await Promise.all([
-    getIndexBoard(),
-    getRecommendPost(),
-  ]);
-  // const newest = await getIndexBoard();
-  // const recommend = await getRecommendPost();
+  // const [newest, recommend] = await Promise.all([
+  //   getIndexBoard(),
+  //   getRecommendPost(),
+  // ]);
+  const newest = await getIndexBoard();
+  const recommend = await getRecommendPost();
 
   return (
     <section>
