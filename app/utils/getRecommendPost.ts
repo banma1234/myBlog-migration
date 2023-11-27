d3201fadcdd9de5f86a9d954fbaf9f999d2348e3
banma1234 <banma1234@gmail.com>
2023-11-27 17:41:12 +0900

    fix: request 헤더에 Content-type 명시

--- app/utils/getRecommendPost.ts
+++ app/utils/getRecommendPost.ts
@@ -2,9 +2,13 @@ import { CardType } from "app/components/componentType";
 
 export default async function getRecommendPost() {
   const URL = process.env.DEV_URL;
+  const myHeaders = new Headers({
+    "Content-Type": "text/html; charset=utf-8",
+  });
 
   const res = await fetch(`${URL}/api/posts/recommend`, {
     method: "GET",
+    headers: myHeaders,
   });
 
   const { data, success }: { data: CardType[] | string; success: boolean } =
