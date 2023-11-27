0b614cb2b5611ac6efd8ea6358304109353ad4d4
banma1234 <banma1234@gmail.com>
2023-11-27 17:06:36 +0900

    refactor: Promise 객체 또다시 통합

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,10 @@ import { CardLayout, Banner } from "./components/card";
 import { getIndexBoard, getRecommendPost } from "./utils";
 
 export default async function Home() {
-  // const [newest, recommend] = await Promise.all([
-  //   getIndexBoard(),
-  //   getRecommendPost(),
-  // ]);
-  const newest = await getIndexBoard();
-  const recommend = await getRecommendPost();
+  const [newest, recommend] = await Promise.all([
+    getIndexBoard(),
+    getRecommendPost(),
+  ]);
 
   return (
     <section>
