437609c5bd0a2aaa0a65ed3ec615ec2c6fcd2fd1
banma1234 <banma1234@gmail.com>
2023-11-29 17:46:28 +0900

    fix: collection명 오타 수정

--- app/api/posts/[...postid]/route.ts
+++ app/api/posts/[...postid]/route.ts
@@ -3,7 +3,7 @@ import { connectToDatabase } from "util/mongodb";
 
 export async function GET(
   req: NextRequest,
-  { params }: { params: { postid: string } }
+  { params }: { params: { postid: string } },
 ) {
   try {
     let postId = params.postid[0];
@@ -37,21 +37,21 @@ export async function GET(
     };
 
     const post = await db
-      .collection("post")
+      .collection("posts")
       .find({ postId: Number(postId) }, options)
       .toArray();
 
     if (!post.length) {
       return NextResponse.json(
         { error: "post not found" },
-        { status: 404, headers: { "Content-Type": "application/json" } }
+        { status: 404, headers: { "Content-Type": "application/json" } },
       );
     }
 
     let recentPosts = undefined;
     try {
       let temp = await db
-        .collection("post")
+        .collection("posts")
         .find({ series: post[0].series, postId: { $ne: postId } }, options2)
         .toArray();
 
@@ -61,10 +61,10 @@ export async function GET(
     }
 
     let both = await db
-      .collection("post")
+      .collection("posts")
       .find(
         { postId: { $in: [Number(postId) + 1, Number(postId) - 1] } },
-        options3
+        options3,
       )
       .toArray();
 
@@ -78,17 +78,17 @@ export async function GET(
 
     return NextResponse.json(
       {
-        post: post,
+        post: post[0],
         recent: recentPosts,
         bothSidePosts: both,
       },
-      { status: 200, headers: { "Content-Type": "application/json" } }
+      { status: 200, headers: { "Content-Type": "application/json" } },
     );
   } catch (e: unknown) {
     console.log(e);
     return NextResponse.json(
       { error: "internal Server Error" },
-      { status: 500, headers: { "Content-Type": "application/json" } }
+      { status: 500, headers: { "Content-Type": "application/json" } },
     );
   }
 }
