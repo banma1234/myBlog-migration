78f466a702b43f2cf0fe87ad77c4ec5f04299d1b
banma1234 <banma1234@gmail.com>
2023-11-29 17:45:02 +0900

    fix: reply에도 admin profile 등록할 수 있도록 수정

--- app/api/comments/POST/addReply.ts
+++ app/api/comments/POST/addReply.ts
@@ -6,6 +6,11 @@ export default async function addReply(req: NextRequest) {
     const { db } = await connectToDatabase();
     const newBody = await req.json();
 
+    if (newBody.isAdmin) {
+      newBody.profile =
+        "https://choco-image-server.cdn.ntruss.com/user/profile/admin.png";
+    }
+
     let lastComment = await db
       .collection("comments")
       .find({
@@ -33,20 +38,20 @@ export default async function addReply(req: NextRequest) {
       },
       {
         $inc: { RE_STEP: 1 },
-      }
+      },
     );
 
     await db.collection("comments").insertOne(newBody);
 
     return NextResponse.json(
       { message: `comment added successfully at ${newBody.postId} : REPLY` },
-      { status: 200, headers: { "Content-Type": "application/json" } }
+      { status: 200, headers: { "Content-Type": "application/json" } },
     );
   } catch (e: unknown) {
     console.log(e);
     return NextResponse.json(
       { error: "internal Server Error" },
-      { status: 500, headers: { "Content-Type": "application/json" } }
+      { status: 500, headers: { "Content-Type": "application/json" } },
     );
   }
 }
