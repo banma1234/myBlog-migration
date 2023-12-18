56f7e2bf22145009e835d17d9e3a6bd90e7dd967
banma1234 <banma1234@gmail.com>
2023-12-18 16:33:30 +0900

    feat: 404 페이지에서 CSR로 데이터 fetching할 때 보여줄 스켈레톤 ui 추가

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { useState, useEffect } from "react";
 import { getRecommendPost } from "./utils";
+import { Suspense } from "react";
 import { CardLayout } from "./components/card";
 import { CardType } from "./components/componentType";
 import "./styles/errorStyle.scss";
@@ -13,8 +14,9 @@ export default function NotFound() {
 
   useEffect(() => {
     const fetchRecommendData = async () => {
-      const res = await getRecommendPost();
-      setRecommend(res);
+      await getRecommendPost().then(res => {
+        setRecommend(res);
+      });
     };
 
     fetchRecommendData();
@@ -28,6 +30,7 @@ export default function NotFound() {
         alt="404 banner"
         width={600}
         height={560}
+        style={{ maxWidth: "100%", minWidth: "28rem" }}
       />
       <div className="error_nav">
         <Link href="/">
@@ -37,12 +40,28 @@ export default function NotFound() {
           <button>검색창 바로가기</button>
         </Link>
       </div>
-      {recommend.length != 0 && (
-        <div>
-          <h2>추천 포스트</h2>
-          <CardLayout posts={recommend} />
-        </div>
-      )}
+      <div>
+        <h2>추천 포스트</h2>
+        {!recommend.length ? <LoadingUi /> : <CardLayout posts={recommend} />}
+      </div>
     </section>
+  );
+}
+
+export function LoadingUi() {
+  return (
+    <div className="container">
+      {new Array(3).fill(1).map(() => {
+        return (
+          <div className="skeleton">
+            <div className="skeleton__thumbnail" />
+            <div className="skeleton__info">
+              <div className="skeleton__article" />
+              <div className="skeleton__description" />
+            </div>
+          </div>
+        );
+      })}
+    </div>
   );
 }
