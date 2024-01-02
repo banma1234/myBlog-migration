01ce6a6ebd4a165c11bb8a1a2572c9abf941a88d
banma1234 <banma1234@gmail.com>
2024-01-02 18:35:04 +0900

    feat: jsx 스타일 추가

--- app/posts/[postId]/utils/mdParser.ts
+++ app/posts/[postId]/utils/mdParser.ts
@@ -10,8 +10,8 @@ import "prismjs/components/prism-json";
 import "prismjs/components/prism-bash";
 import "prismjs/components/prism-typescript";
 import "prismjs/components/prism-python";
+import "prismjs/components/prism-jsx";
 import "prismjs/components/prism-tsx";
-import "prismjs/components/prism-markdown";
 
 export default function mdParser(content: string) {
   /*
