e296cb04ca1776b085513340b80c5fddb85caa7b
chocoham <banma1234@gmail.com>
2024-01-23 17:32:17 +0900

    feat: script 코드 분리 및 특정 키 조합 막는 코드 추가

---
+++ app/utils/globalScript.ts
@@ -0,0 +1,35 @@
+const THEME_SCRIPT = `
+const localStorageTheme = localStorage.getItem("THEME");
+const theme = localStorageTheme || (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
+if (theme === 'dark') {
+  document.documentElement.setAttribute("data-theme", "dark");
+} else {
+  document.documentElement.setAttribute("data-theme", "light")
+}
+`;
+
+const CLIENT_SECURE_SCRIPT = `
+// 특정 키 조합 감지 함수
+function detectKeyCombinations(e) {
+    const forbiddenKeys = ['I', 'J', 'C', 'F12', 'U'];
+    const isForbiddenKey = forbiddenKeys.includes(e.key.toUpperCase()) || forbiddenKeys.includes(e.key);
+
+    if (
+        (e.ctrlKey && e.shiftKey && isForbiddenKey) ||
+        (e.metaKey && e.altKey && isForbiddenKey) ||
+        (e.metaKey && e.shiftKey && e.key.toUpperCase() === 'C') ||
+        e.key === 'F12' ||
+        (e.metaKey && e.altKey && e.key.toUpperCase() === 'U') ||
+        (e.ctrlKey && e.key.toUpperCase() === 'U')
+    ) {
+        e.preventDefault();
+        alert("특정 키 조합이 감지되어 차단되었습니다.");
+    }
+}
+
+window.addEventListener('keydown', detectKeyCombinations);
+`;
+
+const GLOBAL_SCRIPT = THEME_SCRIPT + "\n" + CLIENT_SECURE_SCRIPT;
+
+export default GLOBAL_SCRIPT;
--- app/utils/index.ts
+++ app/utils/index.ts
@@ -1,13 +1,15 @@
 import getIndexBoard from "./getIndexBoard";
 import getRecommendPost from "./getRecommendPost";
 import globalMetaData from "./globalMetaData";
+import GLOBAL_SCRIPT from "./globalScript";
 
 const Components = {
   getIndexBoard,
   getRecommendPost,
   globalMetaData,
+  GLOBAL_SCRIPT,
 };
 
 export default Components;
 
-export { getIndexBoard, getRecommendPost, globalMetaData };
+export { getIndexBoard, getRecommendPost, globalMetaData, GLOBAL_SCRIPT };
