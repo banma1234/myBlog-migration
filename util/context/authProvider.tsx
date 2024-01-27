18387d42a257244bcbc9208e22a6ba3aa433f841
banma1234 <banma1234@gmail.com>
2024-01-27 18:30:10 +0900

    feat: intersectionObserver 분리

--- util/context/authProvider.tsx
+++ util/context/authProvider.tsx
@@ -1,6 +1,9 @@
 "use client";
+
+import useScrollFadeIn from "util/hooks/useScrollFadeIn";
 import { SessionProvider } from "next-auth/react";
 
 export function AuthProviders({ children }: { children: React.ReactNode }) {
+  useScrollFadeIn([".card_layout"]);
   return <SessionProvider>{children}</SessionProvider>;
 }
---
+++ util/hooks/useIntersectionObserver.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { useEffect, useRef } from "react";
+
+export default function useIntersectionObserver({
+  elements,
+  callback,
+  options = {},
+}: UseIntersectionObserverProps): IntersectionObserver | null {
+  const observer = useRef<IntersectionObserver | null>(null);
+
+  useEffect(() => {
+    if (elements.length === 0) return;
+
+    observer.current = new IntersectionObserver(callback, options);
+    console.log("wow", observer.current);
+    elements.forEach(el => observer.current?.observe(el));
+
+    return () => {
+      if (observer.current) {
+        elements.forEach(el => observer.current?.unobserve(el));
+        observer.current.disconnect();
+      }
+    };
+  }, [elements, callback, options]);
+
+  return observer.current;
+}
+
+interface UseIntersectionObserverProps {
+  elements: Element[];
+  callback: IntersectionObserverCallback;
+  options?: IntersectionObserverInit;
+}
